99fde503a74abcddd573861f3e76c83985445eb4
brandonjyuhas <brandonjyuhas@gmail.com>
2014-10-27 11:25:13 -0400

    Add Skill

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027150519) do
+ActiveRecord::Schema.define(version: 20141027151121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "skills", force: true do |t|
+    t.string   "name"
+    t.string   "icon_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
