82bf4b0343a28d82c8c28fd2413ab754846aa2bd
brandonjyuhas <brandonjyuhas@gmail.com>
2014-10-29 14:23:40 -0400

    Create Skill Index

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_experience_points
   has_many :user_quests
+
+  # Figure out which quests have been completed
+  def find_completed
+    completed = self.user_quests.where(complete: true)
+    completed.map do |x|
+      x.quest_id
+    end
+  end
+
+  def find_selected
+    selected_quests = self.user_quests.where(complete: false)
+    selected_quests.map do |x|
+      x.quest_id
+    end
+  end
 end
