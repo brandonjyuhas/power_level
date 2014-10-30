33b23ac1766b76d9bee6e868b712725d9f218007
brandonjyuhas <brandonjyuhas@gmail.com>
2014-10-30 10:27:17 -0400

    Add Level Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  # include Level
+  # Include level module in lib/level.rb
+  include Level
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -14,6 +15,7 @@ class User < ActiveRecord::Base
     end
   end
 
+  # Figure out which quests have been selected (but not completed)
   def find_selected
     selected_quests = self.user_quests.where(complete: false)
     selected_quests.map do |x|
@@ -21,28 +23,8 @@ class User < ActiveRecord::Base
     end
   end
 
+  # Figure out the level, XP to next level, and XP
   def find_level
-    case self.experience_points
-    when 0...1000
-      [1, 1000-self.experience_points, self.experience_points]
-    when 1000...2500
-      [2, 2500-self.experience_points, self.experience_points]
-    when 2500...4500
-      [3, 4500-self.experience_points, self.experience_points]
-    when 4500...7000
-      [4, 7500-self.experience_points, self.experience_points]
-    when 7000...10500
-      [5, 10500-self.experience_points, self.experience_points]
-    when 10500...14500
-      [6, 14500-self.experience_points, self.experience_points]
-    when 14500...20000
-      [7, 20000-self.experience_points, self.experience_points]
-    when 20000...26000
-      [8, 26000-self.experience_points, self.experience_points]
-    when 26000...32500
-      [9, 32500-self.experience_points, self.experience_points]
-    when 32500...29500
-      [10, 1000000000-self.experience_points, self.experience_points]
-    end
+    determine_level(self.experience_points)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module PowerLevel
   class Application < Rails::Application
+
+    # Configure auto loading for modules in lib folder
+    config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
