729538851e40e8fe45ac8bbaa44836f908e4081e
brandonjyuhas <brandonjyuhas@gmail.com>
2014-11-18 14:48:21 -0500

    Add Create to Quests

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -2,4 +2,24 @@ class QuestsController < ApplicationController
   def show
     @quest = Quest.find(params[:id])
   end
+
+  def new
+    @quest = Quest.new
+    @skill = Skill.find(params[:skill])
+  end
+
+  def create
+    @quest = Quest.new(quest_params)
+    if @quest.save
+      redirect_to "/skills/#{@quest.skill_id}"
+    else
+      flash[:alert] = @quest.errors.full_messages
+      redirect_to :back
+    end
+  end
+
+  private
+    def quest_params
+      params.require(:quest).permit(:name, :description, :experience_points, :skill_id)
+    end
 end
--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,4 +1,19 @@
 class Quest < ActiveRecord::Base
+  # Include Icon for icon to experience points converter
+  include Icon
+
   belongs_to :skill
   has_many :user_quests
+  # Find icon after creation
+  after_create :set_icon
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :experience_points, presence: true, numericality: { only_integer: true }
+
+  def set_icon
+    self.icon_url = self.find_icon
+    self.save
+  end
+
 end
